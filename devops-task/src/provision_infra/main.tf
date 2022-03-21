209a372175b6cc3d9340ed24733426834bc8c7cd
jmm288 <joshua.m.barton@gmail.com>
2022-03-21 16:04:08 -0400

    Adding default security group ssh access

--- devops-task/src/provision_infra/main.tf
+++ devops-task/src/provision_infra/main.tf
@@ -92,6 +92,32 @@ resource "aws_security_group" "ecs_sg" {
 
 }
 
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+ 
+  ingress {
+    protocol    = "tcp"
+    self        = false
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_ecr_repository" "php-app" {
     name  = "php-app"
 }
