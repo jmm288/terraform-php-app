503796b671f14aaea164653eefce8bb508186ffb
jmm288 <joshua.m.barton@gmail.com>
2022-03-20 21:51:26 -0400

    removing template file

--- devops-task/src/provision_infra/template_file.tf
+++
@@ -1,6 +0,0 @@
-data "template_file" "task_definition_template" {
-    template = file("task-definition.json.tpl")
-    vars = {
-      REPOSITORY_URL = replace(aws_ecr_repository.php-app.repository_url, "https://", "")
-    }
-}
